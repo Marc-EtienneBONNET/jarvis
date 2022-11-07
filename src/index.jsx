bd2550b8a133ad97f01e3c07e4284f40bbff6282
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-11-07 12:29:15 +0100

    git rapide avec mon make

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './componant/App/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>
 ); 
  
